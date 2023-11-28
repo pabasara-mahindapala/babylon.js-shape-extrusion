8cc78d773364961b9539a93a25a21df148c3eaed
Pabasara Mahindapala <pabasara.mahindapala@bbconsult.co.uk>
2023-11-28 22:00:55 +0530

    drawing shapes

--- app_package/src/Playground/playground.ts
+++ app_package/src/Playground/playground.ts
@@ -1,36 +1,78 @@
 import * as BABYLON from "@babylonjs/core";
 import "@babylonjs/loaders";
+import * as GUI from "@babylonjs/gui";
 
 class Playground {
+    static shapes: any;
+    static isDrawing = false;
+    static camera: BABYLON.ArcRotateCamera;
+
     public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
-        // This creates a basic Babylon Scene object (non-mesh)
         var scene = new BABYLON.Scene(engine);
-
-        // This creates and positions a free camera (non-mesh)
-        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-        // This targets the camera to scene origin
-        camera.setTarget(BABYLON.Vector3.Zero());
-
-        // This attaches the camera to the canvas
-        camera.attachControl(canvas, true);
-
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        this.camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(65), 20, BABYLON.Vector3.Zero(), scene);
+        this.camera.attachControl(canvas, true);
         var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-        // Default intensity is 1. Let's dim the light a small amount
         light.intensity = 0.7;
+        var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 20, height: 20 }, scene);
+        ground.visibility = 0.2;
+        new BABYLON.AxesViewer(scene, 2);
 
-        // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+        var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        advancedTexture.addControl(this.createDrawButton(canvas));
 
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1;
-
-        // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-        var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+        var currentShapePoints: BABYLON.Vector3[] = [];
+        scene.onPointerUp = (event) => {
+            if (!Playground.isDrawing) return;
+            if (event.button == 0) {
+                const pickInfo = scene.pick(scene.pointerX, scene.pointerY);
+                if (pickInfo && pickInfo.hit && pickInfo.pickedPoint) {
+                    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: .1, segments: 16 });
+                    sphere.position.x = pickInfo.pickedPoint.x;
+                    sphere.position.z = pickInfo.pickedPoint.z;
+                    currentShapePoints.push(pickInfo.pickedPoint);
+                }
+            } else {
+                this.drawShape(currentShapePoints, scene);
+                if (!this.shapes) this.shapes = [];
+                this.shapes.push(currentShapePoints);
+                currentShapePoints = [];
+            }
+        };
 
         return scene;
+    }
+
+    static drawShape(shape: BABYLON.Vector3[], scene: BABYLON.Scene) {
+        shape.push(shape[0]);
+        var shapeline = BABYLON.Mesh.CreateLines("sl", shape, scene);
+        shapeline.color = BABYLON.Color3.Green();
+    }
+
+    static createDrawButton(canvas: HTMLCanvasElement): GUI.Control {
+        var drawButton = GUI.Button.CreateSimpleButton("drawBtn", "Draw");
+        drawButton.width = "120px"
+        drawButton.height = "60px";
+        drawButton.color = "white";
+        drawButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        drawButton.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        drawButton.paddingRight = "10px";
+        drawButton.paddingBottom = "10px";
+        drawButton.cornerRadius = 20;
+        drawButton.background = "green";
+        drawButton.onPointerUpObservable.add(function () {
+            if (drawButton.textBlock) {
+                if (Playground.isDrawing) {
+                    drawButton.background = "green";
+                    drawButton.textBlock.text = "Draw";
+                    Playground.isDrawing = false;
+                } else {
+                    drawButton.background = "red";
+                    drawButton.textBlock.text = "Stop";
+                    Playground.isDrawing = true;
+                }
+            }
+        });
+        return drawButton;
     }
 }
 
